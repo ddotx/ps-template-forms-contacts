17a49baea2060298cb792c427cdbdfbe7dfd2ce4
dome_dexpc <domemacintosh@gmail.com>
2024-03-25 01:41:51 +0700

    updated

--- src/app/contacts/in-memory-contacts.service.ts
+++ src/app/contacts/in-memory-contacts.service.ts
@@ -6,9 +6,10 @@ export class InMemoryContactsApi implements InMemoryDbService {
     let contacts: Contact[] = [
       {
         id: '5CehW',
+        personal: false,
         firstName: 'Percival',
         lastName: 'Doodleplumb',
-        dateOfBirth: new Date('1994/05/05'),
+        dateOfBirth: new Date('1994/05/05').toISOString().split('T')[0],
         favoritesRanking: 0,
         phone: { phoneNumber: '555-765-4321', phoneType: 'mobile' },
         address: {
@@ -16,14 +17,16 @@ export class InMemoryContactsApi implements InMemoryDbService {
           city: 'Gleeberg City',
           state: 'Colohoma',
           postalCode: 'A4321',
-          addressType: 'home'
+          addressType: 'home',
         },
+        notes: '',
       },
       {
         id: 'A6rwe',
+        personal: false,
         firstName: 'Mortimer',
         lastName: 'Flungford',
-        dateOfBirth: new Date('1988/10/05'),
+        dateOfBirth: new Date('1988/10/05').toISOString().split('T')[0],
         favoritesRanking: 0,
         phone: { phoneNumber: '555-877-5678', phoneType: 'mobile' },
         address: {
@@ -31,14 +34,16 @@ export class InMemoryContactsApi implements InMemoryDbService {
           city: 'Sleepytown',
           state: 'Ulaska',
           postalCode: 'F2231',
-          addressType: 'other'
+          addressType: 'other',
         },
+        notes: '',
       },
       {
         id: '3bNGA',
+        personal: false,
         firstName: 'Wanda',
         lastName: 'Giggleworth',
-        dateOfBirth: new Date('1986/11/08'),
+        dateOfBirth: new Date('1986/11/08').toISOString().split('T')[0],
         favoritesRanking: 1,
         phone: { phoneNumber: '555-123-4567', phoneType: 'mobile' },
         address: {
@@ -46,11 +51,12 @@ export class InMemoryContactsApi implements InMemoryDbService {
           city: 'Dorado City',
           state: 'Mezona',
           postalCode: 'Z2345',
-          addressType: 'work'
+          addressType: 'work',
         },
+        notes: '',
       },
-    ]
+    ];
 
-    return { contacts }
+    return { contacts };
   }
 }
--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,22 +1,71 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  Contact,
+  addressTypeValues,
+  phoneTypeValues,
+} from '../contacts/contact.model';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  phoneTypes = phoneTypeValues;
+  addressTypes = addressTypeValues;
+  contact: Contact = {
+    id: '',
+    personal: false,
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    favoritesRanking: 0,
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+    notes: '',
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private contactsService: ContactsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) return;
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if (contact) this.contact = contact;
+    });
   }
 
-  saveContact() {
+  /*  saveContact() {
+    this.contactsService.saveContact(this.contact).subscribe({
+      next: () => {
+        this.router.navigate(['/contacts']);
+      },
+    });
+  } */
 
+  saveContact(form: NgForm) {
+    this.contactsService.saveContact(form.value).subscribe({
+      next: () => {
+        this.router.navigate(['/contacts']);
+      },
+    });
   }
 }
